be9603a3c092e112e31a6f5401f38959d12e5aa1
RussianCabbageSoup <testylya333@gmail.com>
2025-11-12 18:07:53 +0300

    debug mode

--- Gacha/Gacha.cpp
+++ Gacha/Gacha.cpp
@@ -2,9 +2,7 @@
 #include <random>
 #include <vector>
 #include <conio.h>
-#include <limits>
 #include <iomanip>
-#include <cmath>
 #include <algorithm>
 
 class GachaSystem {
@@ -32,13 +30,13 @@ private:
         "Skyward Sword", "Wolf's Gravestone", "Jade Cutter", "Prayer to the Sacred Winds", "Amos' Bow",
         "Falcon Sword", "Skyward Pride", "Skyward Spine", "Skyward Atlas", "Skyward Wing" };
 
-    struct dinamicParameters {
-        struct massive {
+    struct dynamicParameters {
+        struct dropLists {
             std::vector <std::pair<std::string, int>> fiveStarDrop;
             std::vector<std::pair<std::string, int>> charDrop;
             std::vector<std::string> inventory;
         };
-        struct number {
+        struct numericValues {
             int countForFourStar = 0;
             int countForFiveStar = 0;
             int rate = 0;
@@ -48,32 +46,44 @@ private:
         };
     };
 
-    dinamicParameters::massive listVal;
-    dinamicParameters::number numberVal;
+    dynamicParameters::dropLists drop_lists;
+    dynamicParameters::numericValues numeric_space;
 
     struct basisParameters {
-        static constexpr double baseFiveStarChance = 0.007;
-        static constexpr double baseFourStarChance = 0.051;
-        static constexpr int startPityValue = 73;
-        static constexpr int fiveStarLimit = 90;
-        static constexpr int fourStarLimit = 10;
-        static constexpr std::pair<double, double> factor = {0.04, 2.85};
-        static constexpr int constLimit = 6;
+        struct baseChances {
+            static constexpr double baseFiveStarChance = 0.007;
+            static constexpr double baseFourStarChance = 0.051;
+            static constexpr double baseEqualChance = 0.5;
+            static constexpr std::pair<double, double> factor = { 0.04, 2.85 };
+        };
+        struct pullValues {
+            static constexpr int startPityValue = 73;
+            static constexpr int fiveStarLimit = 90;
+            static constexpr int fourStarLimit = 10;
+            static constexpr int constLimit = 6;
+        };
+        struct starCurrencyVal{
+            static constexpr int baseBlessForFiveStar = 10;
+            static constexpr int baseBlessForFourStar = 2;
+            static constexpr int limitBlessForFiveStar = 25;
+            static constexpr int limitBlessForFourStar = 5;
+            static constexpr int starDustValue = 18;
+        };
     };
 
     double calcProbability(int currentPull) {
-        return basisParameters::factor.first * currentPull - basisParameters::factor.second;
+        return basisParameters::baseChances::factor.first * currentPull - basisParameters::baseChances::factor.second;
     }
 
     double rateForFiveStar(int currentPull) {
-        if (currentPull == basisParameters::fiveStarLimit) { return 1.0; }
-        if (currentPull >= basisParameters::startPityValue) { return calcProbability(currentPull); }
-        else { return basisParameters::baseFiveStarChance; }
+        if (currentPull == basisParameters::pullValues::fiveStarLimit) { return 1.0; }
+        if (currentPull >= basisParameters::pullValues::startPityValue) { return calcProbability(currentPull); }
+        else { return basisParameters::baseChances::baseFiveStarChance; }
     }
 
     double rateForFourStar(int currentPull) {
-        if (currentPull == basisParameters::fourStarLimit) { return 1.0; }
-        else { return basisParameters::baseFourStarChance; }
+        if (currentPull == basisParameters::pullValues::fourStarLimit) { return 1.0; }
+        else { return basisParameters::baseChances::baseFourStarChance; }
     }
 
     bool checkDuplicate(std::string drop) {
@@ -81,17 +91,17 @@ private:
         bool isLastConst = false;
         size_t index;
 
-        for (size_t i = 0; i < listVal.charDrop.size(); i++) {
-            if (drop == listVal.charDrop[i].first) {
+        for (size_t i = 0; i < drop_lists.charDrop.size(); i++) {
+            if (drop == drop_lists.charDrop[i].first) {
                 duplicate = true;
                 index = i;
             }
         }
         if (!duplicate) {
-            listVal.charDrop.push_back({ drop, 0 });
+            drop_lists.charDrop.push_back({ drop, 0 });
         }
         else {
-            if (listVal.charDrop[index].second < basisParameters::constLimit) { listVal.charDrop[index].second++; }
+            if (drop_lists.charDrop[index].second < basisParameters::pullValues::constLimit) { drop_lists.charDrop[index].second++; }
             else isLastConst = true;
         }
         return isLastConst;
@@ -101,18 +111,18 @@ public:
     GachaSystem() : gen(rd()), charDist(0.0, 1.0) {}
 
     int getPulls() {
-        return numberVal.totalPullCounter; 
+        return numeric_space.totalPullCounter; 
     }
 
     void getCurrency() {
-        std::cout << "Star Bless: " << numberVal.starBless << " (" << numberVal.starBless / 5.0 << ")" << std::endl;
-        std::cout << "Stardust: " << numberVal.stardust << std::endl;
+        std::cout << "Star Bless: " << numeric_space.starBless << " (" << numeric_space.starBless / 5.0 << ")" << std::endl;
+        std::cout << "Stardust: " << numeric_space.stardust << std::endl;
     }
 
     void getInventory() {
-        if (!listVal.inventory.empty()) {
-            for (size_t i = 1; i < listVal.inventory.size() + 1; i++) {
-                std::cout << std::setw(30) << listVal.inventory[i - 1];
+        if (!drop_lists.inventory.empty()) {
+            for (size_t i = 1; i < drop_lists.inventory.size() + 1; i++) {
+                std::cout << std::setw(30) << drop_lists.inventory[i - 1];
                 if (i % 3 == 0) std::cout << std::endl;
             }
         }
@@ -123,12 +133,12 @@ public:
     }
 
     void constellationList() {
-        if (!listVal.charDrop.empty()) {
-            std::sort(listVal.charDrop.begin(), listVal.charDrop.end(), [](const std::pair<std::string, int>& a, const std::pair<std::string, int>& b) {
+        if (!drop_lists.charDrop.empty()) {
+            std::sort(drop_lists.charDrop.begin(), drop_lists.charDrop.end(), [](const std::pair<std::string, int>& a, const std::pair<std::string, int>& b) {
                 return a.second > b.second; });
 
-            for (size_t i = 1; i < listVal.charDrop.size() + 1; i++) {
-                std::cout << std::setw(30) << listVal.charDrop[i - 1].first << " " << listVal.charDrop[i - 1].second;
+            for (size_t i = 1; i < drop_lists.charDrop.size() + 1; i++) {
+                std::cout << std::setw(30) << drop_lists.charDrop[i - 1].first << " " << drop_lists.charDrop[i - 1].second;
                 if (i != 0 && i % 3 == 0) std::cout << std::endl;
             }
         }
@@ -137,23 +147,24 @@ public:
         }
     }
 
-    void getStatistic() {
+    void getStatistic(bool debug) {
 
         double avr = 0;
-        std::vector<int> dist(basisParameters::fiveStarLimit, 0);
+        std::vector<int> dist(basisParameters::pullValues::fiveStarLimit, 0);
 
-        if (!listVal.fiveStarDrop.empty()) {
-            for (size_t i = 1; i < listVal.fiveStarDrop.size() + 1; i++) {
-                std::cout << std::setw(30) << listVal.fiveStarDrop[i - 1].first << " - " << listVal.fiveStarDrop[i - 1].second;
-                if (i != 0 && i % 3 == 0) std::cout << std::endl;
-
-                avr += listVal.fiveStarDrop[i - 1].second;
+        if (!drop_lists.fiveStarDrop.empty()) {
+            for (size_t i = 1; i < drop_lists.fiveStarDrop.size() + 1; i++) {
+                if (!debug) {
+                    std::cout << std::setw(30) << drop_lists.fiveStarDrop[i - 1].first << " - " << drop_lists.fiveStarDrop[i - 1].second;
+                    if (i != 0 && i % 3 == 0) std::cout << std::endl;
+                }
+                avr += drop_lists.fiveStarDrop[i - 1].second;
             }
-            avr /= (double)listVal.fiveStarDrop.size();
+            avr /= (double)drop_lists.fiveStarDrop.size();
             std::cout << std::endl;
 
-            for (size_t i = 0; i < listVal.fiveStarDrop.size(); i++) {
-                dist[listVal.fiveStarDrop[i].second - 1]++;
+            for (size_t i = 0; i < drop_lists.fiveStarDrop.size(); i++) {
+                dist[drop_lists.fiveStarDrop[i].second - 1]++;
             }
         }
         else {
@@ -162,83 +173,87 @@ public:
 
         std::cout << std::endl;
         std::cout << std::setw(15) << "Average: " << avr << std::endl;
-        std::cout << std::setw(15) << "Distribution: " << std::endl;
-        std::cout << std::endl;
 
-        for (size_t i = 1; i < dist.size() + 1; i++) {
-            std::cout << std::setw(8) << i << "-th: " << std::setw(5) << dist[i - 1];
-            if (i != 0 && i % 6 == 0) std::cout << std::endl;
+        if (debug) {
+            std::cout << std::setw(15) << "Distribution: " << std::endl;
+            std::cout << std::endl;
+
+            for (size_t i = 1; i < dist.size() + 1; i++) {
+                std::cout << std::setw(8) << i << "-th: " << std::setw(5) << dist[i - 1];
+                if (i != 0 && i % 6 == 0) std::cout << std::endl;
+            }
         }
     }
 
-    void singleWish() {
+    void singleWish(bool debug = false) {
 
-        numberVal.totalPullCounter++;
-        numberVal.countForFiveStar++;
-        numberVal.countForFourStar++;
-        numberVal.rate++;
+        numeric_space.totalPullCounter++;
+        numeric_space.countForFiveStar++;
+        numeric_space.countForFourStar++;
+        numeric_space.rate++;
 
         double chance = charDist(gen);
 
-        if (chance < rateForFiveStar(numberVal.countForFiveStar)) {
+        if (chance < rateForFiveStar(numeric_space.countForFiveStar)) {
             // Character or Item
-            if (charDist(gen) < 0.5) {  
+            if (charDist(gen) < basisParameters::baseChances::baseEqualChance) {  
                 std::uniform_int_distribution<size_t> dis(0, fiveStarCharacter.size() - 1);
                 std::string drop = fiveStarCharacter[dis(gen)];
-                std::cout << "5-Star " << drop;
+                if (!debug) { std::cout << "5-Star " << drop; }
+                
 
-                if (checkDuplicate(drop)) numberVal.starBless += 25;
-                else numberVal.starBless += 10;
+                if (checkDuplicate(drop)) numeric_space.starBless += basisParameters::starCurrencyVal::limitBlessForFiveStar;
+                else numeric_space.starBless += basisParameters::starCurrencyVal::baseBlessForFiveStar;
 
-                listVal.fiveStarDrop.push_back({ drop, numberVal.rate });
+                drop_lists.fiveStarDrop.push_back({ drop, numeric_space.rate });
 
-                numberVal.countForFiveStar = 0;
-                numberVal.rate = 0;
+                numeric_space.countForFiveStar = 0;
+                numeric_space.rate = 0;
             }
             else {
                 std::uniform_int_distribution<size_t> dis(0, fiveStarItem.size() - 1);
                 std::string drop = fiveStarItem[dis(gen)];
-                std::cout << "5-Star " << drop;
+                if (!debug) { std::cout << "5-Star " << drop; }
 
-                numberVal.starBless += 10;
-                listVal.fiveStarDrop.push_back({ drop, numberVal.rate });
-                listVal.inventory.push_back(drop);
-                numberVal.countForFiveStar = 0;
-                numberVal.rate = 0;
+                numeric_space.starBless += basisParameters::starCurrencyVal::baseBlessForFiveStar;
+                drop_lists.fiveStarDrop.push_back({ drop, numeric_space.rate });
+                drop_lists.inventory.push_back(drop);
+                numeric_space.countForFiveStar = 0;
+                numeric_space.rate = 0;
             }
 
         }
-        else if (chance < rateForFourStar(numberVal.countForFourStar)) {
+        else if (chance < rateForFourStar(numeric_space.countForFourStar)) {
             // Character or Item
-            if (charDist(gen) < 0.5) {    
+            if (charDist(gen) < basisParameters::baseChances::baseEqualChance) {    
                 std::uniform_int_distribution<size_t> dis(0, fourStarCharacter.size() - 1);
                 std::string drop = fourStarCharacter[dis(gen)];
-                std::cout << "4-Star " << drop;
+                if (!debug) { std::cout << "4-Star " << drop; }
 
-                if (checkDuplicate(drop)) numberVal.starBless += 5;
-                else numberVal.starBless += 2;
+                if (checkDuplicate(drop)) numeric_space.starBless += basisParameters::starCurrencyVal::limitBlessForFourStar;
+                else numeric_space.starBless += basisParameters::starCurrencyVal::baseBlessForFourStar;
             }
             else {
                 std::uniform_int_distribution<size_t> dis(0, fourStarItem.size() - 1);
                 std::string drop = fourStarItem[dis(gen)];
-                std::cout << "4-Star " << drop;
+                if (!debug) { std::cout << "4-Star " << drop; }
 
-                numberVal.starBless += 2;
-                listVal.inventory.push_back(drop);
+                numeric_space.starBless += basisParameters::starCurrencyVal::baseBlessForFourStar;
+                drop_lists.inventory.push_back(drop);
             }
 
-            numberVal.countForFourStar = 0;
+            numeric_space.countForFourStar = 0;
         }
         else {
-            std::cout << threeStarItem[0];
-            numberVal.stardust += 18;
+            if (!debug) { std::cout << threeStarItem[0]; }
+            numeric_space.stardust += basisParameters::starCurrencyVal::starDustValue;
         }
     }
 
-    void multiWish(long n) {
+    void multiWish(long n, bool debug = false) {
         for (int i = 0; i < n; i++) {
-            singleWish();
-            std::cout << std::endl;
+            singleWish(debug);
+            if (!debug) { std::cout << std::endl; }
         }
     }
 };
@@ -279,13 +294,25 @@ int main() {
             std::cout << std::endl;
 
             std::cout << "Statistics: " << std::endl;
-            gachaSystem.getStatistic();
+            gachaSystem.getStatistic(false);
             std::cout << std::endl;
         }
+        else if (type == 27) { return 0; } // выход на esc
         // debug
-        /*else {
-            gachaSystem.multiWish(1500000);
-        }*/
+        else {
+            if (type == '0') {
+                int dc = _getch();
+                if (dc == '0') {
+                    std::cout << "// debug //\n(0 to exit)" << std::endl;
+                    long n; std::cin >> n;
+                    if (!n == 0) {
+                        gachaSystem.multiWish(n, true);
+                        gachaSystem.getStatistic(true);
+                    }
+                    else { continue; }
+                }
+            }
+        }
     }
     return 0;
 }
